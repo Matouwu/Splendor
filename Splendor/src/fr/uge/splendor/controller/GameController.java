e584c6e211bf0fea2caa46005910c4b62a4720eb
isabelle-zhan <zhanisabellle@gmail.com>
2025-05-17 19:56:00 +0200

    GameController - setTokenPickaxe.

--- Splendor/src/fr/uge/splendor/controller/GameController.java
+++ Splendor/src/fr/uge/splendor/controller/GameController.java
@@ -3,6 +3,7 @@ package fr.uge.splendor.controller;
 import fr.uge.splendor.model.card.Cards;
 import fr.uge.splendor.model.player.Player;
 import fr.uge.splendor.model.token.Color;
+import fr.uge.splendor.model.token.Token;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -21,6 +22,20 @@ public class GameController {
         this.tokenPickaxe = new HashMap<>();
         this.cardPickaxe = new HashMap<>();
     }
+
+    public void setTokenPickaxe() {
+        switch (playersNumber){
+            case 4:
+                tokenPickaxe.put(Color.GREEN, 7);
+                tokenPickaxe.put(Color.BLUE, 7);
+                tokenPickaxe.put(Color.RED, 7);
+                tokenPickaxe.put(Color.WHITE, 7);
+                tokenPickaxe.put(Color.BLACK, 7);
+                tokenPickaxe.put(Color.YELLOW, 5);
+                break;
+        }
+    }
+
 
     public void addPlayers(Player players) {
         this.players.add(players);
