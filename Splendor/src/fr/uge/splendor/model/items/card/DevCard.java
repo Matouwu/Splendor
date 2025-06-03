cbfe228f29c063bcfa8f213378ff19a944422c17
isabelle-zhan <zhanisabellle@gmail.com>
2025-06-03 17:14:45 +0200

    Update TokenDeck.

--- Splendor/src/fr/uge/splendor/model/items/card/DevCard.java
+++ Splendor/src/fr/uge/splendor/model/items/card/DevCard.java
@@ -1,16 +1,25 @@
 package fr.uge.splendor.model.items.card;
 
 import fr.uge.splendor.model.items.Color;
-import java.util.Map;
+import fr.uge.splendor.model.items.deck.TokenDeck;
+
 import java.util.Objects;
 
-public record DevCard(int level, int prestigePoints, Map<Color, Integer> tokenRequire, Color tokenReduction, String illustration) {
-	
+public record DevCard(int level, int prestigePoints, TokenDeck tokenRequire, Color tokenReduction, String illustration) {
     public DevCard {
         if (level < 0 ||prestigePoints < 0) throw new IllegalArgumentException();
         Objects.requireNonNull(tokenRequire);
         Objects.requireNonNull(tokenReduction);
         Objects.requireNonNull(illustration);
+    }
+
+    @Override
+    public String toString() {
+        return "DevCard [level=" + level +
+                ", prestigePoints=" + prestigePoints +
+                ", tokenRequire=" + tokenRequire +
+                ", tokenReduction=" + tokenReduction +
+                ", illustration=" + illustration + "]";
     }
    
 }
--- Splendor/src/fr/uge/splendor/model/items/deck/DevDeck.java
+++ Splendor/src/fr/uge/splendor/model/items/deck/DevDeck.java
@@ -1,80 +1,33 @@
 package fr.uge.splendor.model.items.deck;
 
+
+import fr.uge.splendor.model.items.card.DevCard;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
-import fr.uge.splendor.model.items.Color;
-import fr.uge.splendor.model.items.Token;
-
-import java.util.Objects;
 
 public class DevDeck {
-	
-	private Map<Color, Integer> tokenRequire = new HashMap<>();
-    private final Color tokenReduction;
-    private final int prestigePoints;
-    private final int level;
+    private List<DevCard> devDeck = new ArrayList<>();
 
-    public DevDeck(int level, int prestigePoints, Map<Color, Integer> tokenRequire, Color tokenReduction ) {
-        if(prestigePoints < 0 || level < 0) throw new IllegalArgumentException();
-        Objects.requireNonNull(tokenRequire);
-        Objects.requireNonNull(tokenReduction);
-
-        this.level = level;
-        this.prestigePoints = prestigePoints;
-        this.tokenRequire = tokenRequire;
-        this.tokenReduction = tokenReduction;
-    }
-
-    private static Map<Color, Integer> convertTokenListToCosts(List<Token> tokens) {
-        Map<Color, Integer> costs = new HashMap<>();
-        
-        for (Color color : Color.values()) {
-            costs.put(color, 0);
-        }
-        
-        for (Token token : tokens) {
-            Color color = token.color();
-            var quantity = token.number();
-            costs.put(color, costs.get(color) + quantity);
-        }
-        
-        return costs;
-    }
-    
-    
-    public static List<Token> convertCostsToTokenList(Map<Color, Integer> costs) {
-        List<Token> tokens = new ArrayList<>();
-        
-        for (Map.Entry<Color, Integer> entry : costs.entrySet()) {
-            if (entry.getValue() > 0) {
-                tokens.add(new Token(entry.getKey(), entry.getValue()));
-            }
-        }
-        
-        return tokens;
-    }
-    
-    
-    public boolean canBePurchasedBy(Map<Color, Integer> playerTokens, Map<Color, Integer> costs) {
-     
-    		for (Map.Entry<Color, Integer> cost : costs.entrySet()) {
-    		Color color = cost.getKey();
-    		var required = cost.getValue();
-    		var available = playerTokens.getOrDefault(color, 0) + costs.getOrDefault(color, 0);
-
-    		if (available < required) {
-    				return false;
-    			}
-    		}
-    		return true ;
-    }
-    
-    
     @Override
     public String toString() {
         return "DevDeck : [level= " + level + "; prestigePoints= " + prestigePoints + "; tokenRequire= " + tokenRequire + "; tokenReduction= " + tokenReduction + "]";
+    }
+
+    public boolean isEmpty() {
+        return devDeck.isEmpty();
+    }
+
+    public void setListDevCardDeck() {
+        for (var card : devDeck) {
+            devDeck.add(new DevCard(card.tokenRequire()));
+        }
+        Collections.shuffle(devCard);
+    }
+
+    public CardsP1 drawDevCardDeck() {
+        if(devCard.isEmpty()) throw new IllegalStateException("DevCards is empty");
+        return devCard.removeFirst();
     }
 }
